75023c165aac0d4c27adff9469bb70667d4b1197
SvRafael <rafaelsvsouza@hotmail.com>
2026-09-15 20:11:45 -0300

    feat: projeto final

--- src/App.jsx
+++ src/App.jsx
@@ -36,16 +36,29 @@ function App() {
       </header>
       <Banner />
       <FormularioDeEvento temas={temas} aoSubmeter={adicionarEvento}/>
-      {temas.map((item) => {
-        return (
-          <section key={item.id}>
-            <Tema tema={item}/>
-            {eventos.map((item, index) => {
-              return <Card evento={item} key={index}/>
-            })}
-          </section>
-        )
-      })}
+      <section className="container">
+        {temas.map((tema) => {
+          if(!eventos.some(function(evento){
+            return evento.tema.id == tema.id
+          })){
+            return null
+          }
+          return (
+            <section key={tema.id}>
+              <Tema tema={tema}/>
+              <div className="eventos">
+                {eventos
+                .filter((evento) => {
+                  return evento.tema.id == tema.id
+                })
+                .map((evento, index) => {
+                  return <Card evento={evento} key={index}/>
+                })}
+              </div>
+            </section>
+          )
+        })}
+      </section>
     </main>
   )
 }
